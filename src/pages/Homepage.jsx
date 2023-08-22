a3ca2044e0e61439aa61063af6f2f36ef90eecd6
brigihorvath <brigitta.horvath@lego.com>
2023-08-22 16:40:46 +0200

    Router started

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>World Map App</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
+};
+
+export default Homepage;
